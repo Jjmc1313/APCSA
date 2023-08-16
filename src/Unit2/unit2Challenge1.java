144e8d4238617fb228c1d00eb8e3ab3e66e9073d
J_jmc <johnlhartshorne@gmail.com>
2023-08-16 00:17:52 -0700

    Updated ReadMe and finished Unit2Challenge1

--- src/Unit2/unit2Challenge1.java
+++ src/Unit2/unit2Challenge1.java
@@ -2,15 +2,20 @@ package Unit2;
 public class unit2Challenge1 {
     public static void main(String[] args) {
         final String BASE = "computational";
+        final String BASE2 = "Hypothetical";
         System.out.println(locate("tip", BASE));
         System.out.println(locate("it", BASE));
         System.out.println(locate("opinion", BASE));
         System.out.println(locate("atomic", BASE));
         System.out.println(locate("Banana", BASE));
+
+        System.out.println("\n"+locate("ache", BASE2));
+        System.out.println(locate("Test", BASE2));
     }
 
     private static String locate (String word, String base) {
         word = word.toLowerCase();
+        base = base.toLowerCase();
         StringBuilder sb = new StringBuilder();
         String output = "";
         for (int i = 0; i < word.length(); i++) {
