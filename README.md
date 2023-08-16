3ed7e7537c31167112175c83a172fcfc51dfa548
J_jmc <johnlhartshorne@gmail.com>
2023-08-16 00:05:14 -0700

    Updated ReadMe and finished Unit2Challenge1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # APCSA Repository
 This is where I will upload Challenge Problems, as well as Extra Credit Problems.
-- All Challenge Problems will be labeled according to unit and order. (e.g. unit1Challenge3, unit1Challenge4, etc)
+- All Challenge Problems will be labeled according to unit and order. (e.g. unit1Challenge3, unit1Challenge4, etc.)
 - Extra Credit Problems will be labeled according to their function.
 
 This repository, as long as my other repos, can be accessed from [This Link](http://jhart.us.to)
--- src/Unit2/unit2Challenge1.java
+++ src/Unit2/unit2Challenge1.java
@@ -1,6 +1,25 @@
 package Unit2;
 public class unit2Challenge1 {
     public static void main(String[] args) {
-        // Unsure of instructions.
+        final String BASE = "computational";
+        System.out.println(locate("tip", BASE));
+        System.out.println(locate("it", BASE));
+        System.out.println(locate("opinion", BASE));
+        System.out.println(locate("atomic", BASE));
+        System.out.println(locate("Banana", BASE));
+    }
+
+    private static String locate (String word, String base) {
+        word = word.toLowerCase();
+        StringBuilder sb = new StringBuilder();
+        String output = "";
+        for (int i = 0; i < word.length(); i++) {
+            char ch = 0;
+            try {ch = base.charAt(base.lastIndexOf(word.substring(i, i + 1)));
+            } catch (Exception e) {System.out.println("Error Finding Character: " + (word.charAt(i)));}
+            sb.appendCodePoint(ch);
+            output = sb.toString();
+        }
+        return output;
     }
 }
