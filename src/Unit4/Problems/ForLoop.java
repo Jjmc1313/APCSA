de8cf6a24a1f583ee3e8dfc14e10379cdf604d9e
J_jmc <87952311+Jjmc1313@users.noreply.github.com>
2023-08-30 14:48:18 -0700

    Add files via upload

---
+++ src/Unit4/Problems/ForLoop.java
@@ -0,0 +1,19 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            System.out.print("How many lines ");
+            System.out.println("are printed?");
+        }
+
+        int total = 25;
+        for (int number = 1; number <= (total/2); number++) {
+            total = total - number;
+            System.out.println(total + " " + number);
+        }
+        System.out.println("T-minus");
+        for (int i = 5; i >= 1; i--) {
+            System.out.print(i + ",");
+        }
+        System.out.println("Blastoff!");
+    }
+}
