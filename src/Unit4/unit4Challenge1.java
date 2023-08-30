6624a99ded06d26e73794e38dbad5dabfcf94cea
J_jmc <87952311+Jjmc1313@users.noreply.github.com>
2023-08-30 14:56:54 -0700

    Update unit4Challenge1.java

--- src/Unit4/unit4Challenge1.java
+++ src/Unit4/unit4Challenge1.java
@@ -1,1 +1,34 @@
+import java.io.PrintStream;
 
+public class unit4Challenge1 {
+    public static void main(String[] args) {
+        for (int i = 5; i <= 30; i+=5) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 4; i <= 79; i+=15) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 30; i >= -20; i-=10) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = -7; i <= 13; i+=4) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 97; i >=82; i-=3) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = -4; i <= 86; i+=18) {
+            System.out.print(i + " ");
+        }
+    }
+}
