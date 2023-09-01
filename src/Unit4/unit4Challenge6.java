dcb62a3b0718313ab226142782f99d65e1020480
J_jmc <87952311+Jjmc1313@users.noreply.github.com>
2023-09-01 12:46:03 -0700

    Update unit4Challenge6.java

--- src/Unit4/unit4Challenge6.java
+++ src/Unit4/unit4Challenge6.java
@@ -1,45 +1,46 @@
-package Unit4;
-
+package unit4;
 import java.util.Scanner;
 
 public class unit4Challenge6 {
     public static void main(String[] args) {
         float total = 0;
         Scanner scan = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("How many classes do you have?");
         int classes = scan.nextInt();
 
-        for (int i = 0; i < classes; i++) {
-            System.out.print("Class" + i + ":");
-            String grade = scan.nextLine();
+        String[] grade = new String[classes];
 
-            if (grade.equalsIgnoreCase("A+")) {
+        for (int i = 0; i < classes; i++) {
+            System.out.print("Grade: ");
+            grade[i] = input.next();
+
+            if (grade[i].equalsIgnoreCase("A+")) {
                 total += 4.0F;
-            } else if (grade.equalsIgnoreCase("A")) {
+            } else if (grade[i].equalsIgnoreCase("A")) {
                 total += 4.0F;
-            } else if (grade.equalsIgnoreCase("A-")) {
+            } else if (grade[i].equalsIgnoreCase("A-")) {
                 total += 3.7F;
-            } else if (grade.equalsIgnoreCase("B+")) {
+            } else if (grade[i].equalsIgnoreCase("B+")) {
                 total += 3.3F;
-            } else if (grade.equalsIgnoreCase("B")) {
+            } else if (grade[i].equalsIgnoreCase("B")) {
                 total += 3.0F;
-            } else if (grade.equalsIgnoreCase("B-")) {
+            } else if (grade[i].equalsIgnoreCase("B-")) {
                 total += 2.7F;
-            } else if (grade.equalsIgnoreCase("C+")) {
+            } else if (grade[i].equalsIgnoreCase("C+")) {
                 total += 2.3F;
-            } else if (grade.equalsIgnoreCase("C")) {
+            } else if (grade[i].equalsIgnoreCase("C")) {
                 total += 2.0F;
-            } else if (grade.equalsIgnoreCase("C-")) {
+            } else if (grade[i].equalsIgnoreCase("C-")) {
                 total += 1.7F;
-            } else if (grade.equalsIgnoreCase("D+")) {
+            } else if (grade[i].equalsIgnoreCase("D+")) {
                 total += 1.3F;
-            } else if (grade.equalsIgnoreCase("D")) {
+            } else if (grade[i].equalsIgnoreCase("D")) {
                 total += 1.0F;
-            } else if (grade.equalsIgnoreCase("F")) {
+            } else if (grade[i].equalsIgnoreCase("F")) {
                 total += 0F;
             }
         }
-
-        System.out.println("GPA:\n" + (Math.round((total/classes)) * 100) / 100);
+        System.out.println("GPA:" + (double)(Math.round((total/classes)) * 100) / 100);
     }
 }
