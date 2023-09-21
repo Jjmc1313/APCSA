757cc2b3aa403f8f49eb19505d3e0ff0a8724498
J_jmc <johnlhartshorne@gmail.com>
2023-09-20 23:07:00 -0700

    U4C8 Finished, Remove debuggers and tidy up

--- src/Unit4/unit4Challenge8.java
+++ src/Unit4/unit4Challenge8.java
@@ -14,13 +14,25 @@ public class unit4Challenge8 {
         int number;
         String color;
         String val;
+        int[] redVals = {1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36};
 
         boolean escape = false;
-
         while (!escape) {
-            int roulette = random(-1, 5); //-1 = 00
+            int roulette = random(-1, 36); //-1 = 00
             System.out.println("[DEBUG] Roulette: " + roulette);
-
+            boolean isRed = false;
+            boolean zero = false;
+            if (roulette > 0) {
+                for (int i = 0; i < 18; i++){
+                    if (roulette == redVals[i]) {
+                        isRed = true;
+                    }
+                    // System.out.print(isRed + " ");
+                }
+                // System.out.println("\n");
+            } else {
+                zero = true;
+            }
             while (!validBet) {
                 System.out.println("What is your bet?");
                 bet = betManager.nextInt();
@@ -31,40 +43,67 @@ public class unit4Challenge8 {
                     validBet = true;
                 }
             }
-
             System.out.println("What are you betting against?");
             betting = scan.nextLine();
             if (betting.equalsIgnoreCase("number")) {
                 System.out.println("What number do you want to bet on?");
                 number = scan.nextInt();
                 if (number == roulette) {
-                    userMoney+=(35F/bet);
+                    userMoney+=bet;
                     System.out.print("[DEBUG] Win | UserMoney: $" + userMoney);
                 } else {
                     userMoney-=bet;
                     System.out.print("[DEBUG] Lose | UserMoney: $" + userMoney);
                 }
-
             } else if (betting.equalsIgnoreCase("color")) {
                 System.out.println("What color are you betting on?");
                 color = scan.nextLine();
+
                 if (color.equalsIgnoreCase("red")) {
-                    if (roulette == 1 || roulette == 3 || roulette == 5) { // FIND ANOTHER WAY OF DOING THIS
-
+                    if (isRed) {
+                        userMoney+=bet;
+                        System.out.println("[DEBUG] Win | UserMoney: $" + userMoney);
+                    } else {
+                        userMoney-=bet;
+                        System.out.println("[DEBUG] Lose | UserMoney: $" + userMoney);
                     }
+                } else if (color.equalsIgnoreCase("black")) {
+                    if (isRed) {
+                        userMoney-=bet;
+                        System.out.println("[DEBUG] Lose | UserMoney: $" + userMoney);
+                    } else {
+                        userMoney+=bet;
+                        System.out.println("[DEBUG] Win | UserMoney: $" + userMoney);
+                    }
+                } if (zero) {
+                    System.out.println("[DEBUG] Received Zero | UserMoney: $" + userMoney);
                 }
-
             } else if (betting.equalsIgnoreCase("count")) {
                 System.out.println("Betting high or low?");
                 val = scan.nextLine();
-
+                if (val.equalsIgnoreCase("low")) {
+                    if (roulette >= 1 && roulette <= 18) {
+                        userMoney+=bet;
+                        System.out.println("[DEBUG] Win | UserMoney: $" + userMoney);
+                    } else {
+                        userMoney-=bet;
+                        System.out.println("[DEBUG] Lose | UserMoney: $" + userMoney);
+                    }
+                } else if (val.equalsIgnoreCase("high")) {
+                    if (roulette >= 19 && roulette <= 36) {
+                        userMoney+=bet;
+                        System.out.println("[DEBUG] Win | UserMoney: $" + userMoney);
+                    } else {
+                        userMoney-=bet;
+                        System.out.println("[DEBUG] Lose | UserMoney: $" + userMoney);
+                    }
+                }
             } else if (betting.equalsIgnoreCase("quit")) {
                 escape = true;
             }
             validBet = false;
         }
     }
-
     private static int random(int lowerBound, int upperBound) {
         Random rnd = new Random();
         int randV = rnd.nextInt(upperBound - lowerBound + 1) + lowerBound;
