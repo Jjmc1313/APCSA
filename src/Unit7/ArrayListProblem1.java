1636dcac9bf9517c3b4b752f4e8bf9270d2e4052
J_jmc <87952311+Jjmc1313@users.noreply.github.com>
2023-10-19 12:28:10 -0700

    Create ArrayListProblem1.java

---
+++ src/Unit7/ArrayListProblem1.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class ArrayListProblem1 {
+  public static void main(String[] args) {
+    ArrayList<String> a1 = new ArrayList<String>();
+
+    a1.add("Lakers");
+    a1.add("Kings");
+    a1.add("Rockets");
+    a1.add("Suns");
+    a1.add("Warriors");
+    System.out.println("Elements of ArrayList of String Type: " + a1);
+
+    ArrayList<Integer> a2 = new ArrayList<Integer>();
+    a2.add(11);
+    a2.add(34);
+    a2.add(99);
+    a2.add(99);
+    a2.add(78);
+    System.out.println("Elements of ArrayList of Integer Type: " + a2);
+  }
+}
+    
