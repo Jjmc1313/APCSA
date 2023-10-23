1252e9bce1396afb0a2d14c171101c8155fef9e1
J_jmc <87952311+Jjmc1313@users.noreply.github.com>
2023-10-23 11:51:48 -0700

    Update ArrayListProblem7.java

--- src/Unit7/ArrayListProblem7.java
+++ src/Unit7/ArrayListProblem7.java
@@ -1,7 +1,20 @@
-import java.util.ArrayList;
-
+import java.util.*;
 public class ArrayListProblem7 {
-  public static void main(String[] args) {
-    
-  }
+    public static void main(String[] args) {
+        ArrayList<String> al = new ArrayList<String>();
+        al.add("James");
+        al.add("Bella");
+        al.add("Susan");
+        al.add("Thomas");
+        System.out.println("ArrayList1 ebfore addAll:"+al);
+
+        ArrayList<String> al2 = new ArrayList<String>();
+        al2.add("Jerry");
+        al2.add("Steve");
+        al2.add("Terry");
+        al2.add("Ella");
+        
+        al.addAll(al2);
+        System.out.println("ArrayList1 after addAll:"+al);
+    }
 }
