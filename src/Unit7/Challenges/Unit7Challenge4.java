0aeb826780a886272818db875bacf202c8db271e
J_jmc <87952311+Jjmc1313@users.noreply.github.com>
2023-11-01 13:26:40 -0700

    Update Unit7Challenge4.java

--- src/Unit7/Challenges/Unit7Challenge4.java
+++ src/Unit7/Challenges/Unit7Challenge4.java
@@ -7,49 +7,70 @@ public class Unit7Challenge4 {
         ArrayList<String> names = new ArrayList<String>();
         ArrayList<Integer> scores = new ArrayList<Integer>();
 
-        boolean quit = false;
-        while (!quit) {
-            System.out.print("Input a name: ");
-            String name = scan.nextLine();
-            if (name.equalsIgnoreCase("quit")) {
-                quit = true;
+        boolean full = false;
+        while (!full) {
+            boolean quit = false;
+            while (!quit) {
+                System.out.print("Input a name: ");
+                String name = scan.nextLine();
+                if (name.equalsIgnoreCase("quit")) {
+                    quit = true;
+                } else {
+                    names.add(name);
+                    System.out.print("Input Score: ");
+                    int score = scan1.nextInt();
+                    scores.add(score);
+                }
+            }
+
+            System.out.println();
+            int i = 0;
+            for (String s : names) {
+                String letter = "A";
+                if ((double)scores.get(i) / 100 >= 0.9F) {
+                    letter = "A";
+                } else if ((double)scores.get(i) / 100 >= 0.8F) {
+                    letter = "B";
+                } else if ((double)scores.get(i) / 100 >= 0.7F) {
+                    letter = "C";
+                } else if ((double)scores.get(i) / 100 >= 0.6F) {
+                    letter = "D";
+                } else if ((double)scores.get(i) / 100 < 0.6F) {
+                    letter = "F";
+                }
+                System.out.print(s + " " + scores.get(i) + " " + letter + "\n");
+                i++;
+            }
+
+            int total = 0;
+            for (int in : scores) {
+                total += in;
+            }
+            System.out.println("Average: " + (double)total/(scores.size()-1) + "\n");
+
+            Collections.sort(scores);
+            System.out.println("Highest: " + scores.get(scores.size()-1));
+            System.out.println("Lowest: " + scores.get(0) + "\n");
+
+            int[] occ = new int[101];
+            for (int a : scores) {
+                occ[a]+=1;
+            }
+            int o = 0;
+            for (int a : occ) {
+                if (a != 0) {
+                    System.out.println(o + " " + a);
+                }
+                o++;
+            }
+
+            System.out.println("Would you like to make changes? ");
+            String changes = scan.nextLine();
+            if (changes.equalsIgnoreCase("yes")) {
+                full = false;
             } else {
-                names.add(name);
-                System.out.print("Input Score: ");
-                int score = scan1.nextInt();
-                scores.add(score);
+                full = true;
             }
         }
-
-        System.out.println();
-        int i = 0;
-        for (String s : names) {
-            System.out.print(s + " " + scores.get(i) + "\n");
-            i++;
-        }
-
-        int total = 0;
-        for (int in : scores) {
-            total += in;
-        }
-        System.out.println("Average: " + (double)total/(scores.size()-1) + "\n");
-
-        Collections.sort(scores);
-        System.out.println("Highest: " + scores.get(scores.size()-1));
-        System.out.println("Lowest: " + scores.get(0) + "\n");
-
-        int[] occ = new int[100];
-        for (int a : scores) {
-            occ[a]+=1;
-        }
-        int o = 0;
-        for (int a : occ) {
-            if (a != 0) {
-                System.out.println(o + " " + a);
-            }
-            o++;
-        }
-        int[] scoreNew = new int[scores.size()];
-        String namesNew = Arrays.toString(new String[names.size()]);
     }
 }
