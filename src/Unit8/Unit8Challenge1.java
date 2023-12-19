5918eac64151ead76ec4e04f6ce061f9a3c46625
J_jmc <87952311+Jjmc1313@users.noreply.github.com>
2023-12-18 23:33:24 -0800

    Create Unit8Challenge1.java

---
+++ src/Unit8/Unit8Challenge1.java
@@ -0,0 +1,16 @@
+
+
+public class Unit8Challenge1 {
+    public static void main(String[] args) {
+        String[][] colors = new String[][]{{"red", "yellow", "blue"}, {"orange", "green", "purple"}};
+
+        for (String s : colors[0]) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
+        for (String s : colors[1]) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
+    }
+}
